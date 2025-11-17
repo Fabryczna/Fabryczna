f2adad316eff17ffa32b705ffce2a7ff9c7852ef
Mete Yalcinkaya <ymete089@gmail.com>
2025-11-17 15:41:17 +0100

    Fix: Add basename to BrowserRouter for GitHub Pages routing

--- shadcn-ui/src/App.tsx
+++ shadcn-ui/src/App.tsx
@@ -11,7 +11,7 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
-      <BrowserRouter>
+      <BrowserRouter basename="/Fabryczna">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
